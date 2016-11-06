b8a380e0c0d7d6ac402c531e2331c2c4baf0f063
ehedei206 <ehedei206@msn.com>
2016-11-06 16:56:17 +1300

    validation

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,10 @@ get '/' do
   erb :index
 end
 
+get '/:error' do
+  erb :index
+end
+
 get '/anagrams/:word' do
   @word = params[:word]
   @anagrams = Word.find_anagrams(@word)
@@ -11,6 +15,39 @@ end
 
 
 post '/' do
+ 
   @word = params[:word]
-  redirect "/anagrams/#{@word}"
+  if valid_input?(@word)
+      redirect "/anagrams/#{@word}"
+    else
+    	@error="Word is longer than 3 letters or it has duplicate letters"
+    	redirect "/#{@error}"
+    end
+end
+
+def three_letters?(input)
+	if input.size<4
+		return true
+	else
+		return false
+	end
+
+end
+
+def distinct_letters?(input)
+   letter_array = input.chars
+   unique_letters = letter_array.uniq
+   if unique_letters.length < letter_array.length
+     false
+   else
+     true
+   end
+end
+
+def valid_input?(input)
+    if three_letters?(input) && distinct_letters?(input)
+      true
+    else
+      false
+    end
 end
